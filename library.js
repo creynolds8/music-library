8352f52b3c0fc1cbe3fb4e6f588bd03fd3f054c2
creynolds8 <cameron8.cr@gmail.com>
2024-05-03 19:52:29 -0700

    copy functions into element

--- library.js
+++ library.js
@@ -30,7 +30,72 @@ const library = {
       name: "Other Playlist",
       tracks: ["t03"]
     }
+  },
+  trackCounter: playlist => bkmkLP[playlist].tracks.length,
+
+  printPlaylists: function() {
+  const playlistArr = Object.keys(bkmkLP);
+  playlistArr.forEach(pL => console.log(`${pL}: ${bkmkLP[pL].name} - ${trackCounter(pL)} tracks`));
+},
+
+printTracks: function() {
+  const trackArr = Object.keys(bkmkLT);
+  trackArr.forEach(trackNum => console.log(`${trackNum}: ${bkmkLT[trackNum].name} by ${bkmkLT[trackNum].artist} (${bkmkLT[trackNum].album})`
+  ));
+},
+
+printPlaylist: function(playlistId) {
+  for (const playlist in bkmkLP) {
+    if (playlist === playlistId) {
+      console.log(`${playlist}: ${bkmkLP[playlist].name} - ${trackCounter(playlist)} tracks`);
+      //console.log(playlist) //-> p01
+      for (const track of bkmkLP[playlist].tracks) {
+        //console.log(track); // -> t01 /n t02
+        for (const trackID in bkmkLT) {
+          if (bkmkLT[trackID].id === track) {
+            console.log(`${track}: ${bkmkLT[trackID].name} by ${bkmkLT[trackID].artist} (${bkmkLT[trackID].album})`);
+          }
+        }
+      }
+    }
   }
+},
+
+addTrackToPlaylist: function(trackId, playlistId) {
+  for (const trackName in bkmkLT) {
+    if (trackName === trackId) {
+      bkmkLP[playlistId].tracks.unshift(trackId);
+    }
+  }
+  console.log(`Playlist now has ${bkmkLP[playlistId].tracks.length} tracks`);
+
+},
+
+generateUid: function() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+},
+
+addTrack: function(name, artist, album) {
+  const newId = generateUid();
+  library.tracks[newId] = {
+    id: newId,
+    name: name,
+    artist: artist,
+    album: album
+  };
+  console.log('New Song added!', bkmkLT[newId]);
+},
+
+addTrackToPlaylist: function(trackId, playlistId) {
+  for (const trackName in bkmkLT) {
+    if (trackName === trackId) {
+      bkmkLP[playlistId].tracks.unshift(trackId);
+    }
+  }
+  console.log(`Playlist now has ${bkmkLP[playlistId].tracks.length} tracks`);
+
+}
+
 };
 
 /////////////////////////////
@@ -43,15 +108,12 @@ const library = {
 
 const divider = '----------------------';
 
-const bkmkLP = library.playlists;
-const bkmkLT = library.tracks;
+const bkmkLP = this.playlists;
+const bkmkLT = this.tracks;
 
 const trackCounter = playlist => bkmkLP[playlist].tracks.length;
 
-const printPlaylists = function() {
-  const playlistArr = Object.keys(bkmkLP);
-  playlistArr.forEach(pL => console.log(`${pL}: ${bkmkLP[pL].name} - ${trackCounter(pL)} tracks`));
-};
+
 printPlaylists();
 console.log(divider);
 
@@ -59,11 +121,7 @@ console.log(divider);
 // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
-const printTracks = function() {
-  const trackArr = Object.keys(bkmkLT);
-  trackArr.forEach(trackNum => console.log(`${trackNum}: ${bkmkLT[trackNum].name} by ${bkmkLT[trackNum].artist} (${bkmkLT[trackNum].album})`
-  ));
-};
+
 
 printTracks();
 console.log(divider);
